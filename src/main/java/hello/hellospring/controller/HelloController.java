23fefc31f5cd06bdbf55d7dc3c64532e4e02c9ea
ryudongan <90627763+yoodongan@users.noreply.github.com>
2022-07-11 00:00:31 +0900

    initial commit

---
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -0,0 +1,25 @@
+package hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "유동안입니다.");
+        return "hello";
+    }
+    @GetMapping("hello-mvc")
+    public String helloMVC(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+    @GetMapping("hello-string")
+    public String helloString(@RequestParam("name") String name) {
+        return "hello" + name;
+    }
+
+}
